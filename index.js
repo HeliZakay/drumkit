dad9e5ea310106293a4abfa3af11bbde47bb4faa
HeliZakay <64401701+HeliZakay@users.noreply.github.com>
2020-07-22 19:12:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+//Detecting button press
+var buttonsArray = document.querySelectorAll(".drum");
+for (var i = 0; i < buttonsArray.length; i++) {
+  buttonsArray[i].addEventListener("click", function() {
+    playSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
+}
+//Detecting keyboard press
+document.addEventListener("keydown", function(event) {
+  playSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+function playSound(keyPressed) {
+  switch (keyPressed) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log(this.innerHTML);
+  }
+}
+
+function buttonAnimation(keyPressed) {
+  var activeButton = document.querySelector("." + keyPressed);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
